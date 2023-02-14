d9a8c3747486953eb5cd663ca260af1a4e4365fb
Oleksandr Samsonenko <invizibji@gmail.com>
2023-02-14 19:15:14 +0200

    console.log removed

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -35,6 +35,5 @@ Statistics.propTypes = {
   ),
 };
 function getRandomColor() {
-  console.log(Math.floor(Math.random() * 16777215).toString(16));
   return Math.floor(Math.random() * 16777215).toString(16);
 }
